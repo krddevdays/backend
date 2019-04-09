fe52b62b2d32515b818aa53075b8096a83e4e117
TyVik <tyvik8@gmail.com>
2019-04-09 19:25:48 +0300

    Add link to reset password.

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from users.models import User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     pass
