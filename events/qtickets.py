67c231a033f4754746f32de1ef0d8f21419d1f79
sergeibersadsky <sergeibershadsky@gmail.com>
2019-04-27 22:40:37 +0300

    make class out of qtickets check event id  method

--- events/qtickets.py
+++ events/qtickets.py
@@ -2,9 +2,12 @@ import requests
 from django.conf import settings
 
 
-def check_qtickets_event(external_id: int):
-    """ Check qtickets.com system for this event id by calling their API """
-
+class QTickets:
     headers = {'Authorization': f'Bearer {settings.QTICKETS_TOKEN}'}
-    endpoint = f'{settings.QTICKETS_ENDPOINT}/api/rest/v1/events/{external_id}'
-    requests.head(endpoint, headers=headers).raise_for_status()
+    endpoint = f'{settings.QTICKETS_ENDPOINT}/api/rest/v1/events/'
+
+    @classmethod
+    def check_event_exist(cls, external_id: int):
+        """ Check qtickets.com system for this event id by calling their API """
+
+        requests.head(f'{cls.endpoint}{external_id}', headers=cls.headers).raise_for_status()
