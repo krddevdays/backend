138ada6ca71a07f77345d843fe3a00cfc2fdf38f
Mark Langovoi <marklangovoi@gmail.com>
2019-04-27 14:08:42 +0300

    Configure CORS

--- krddevdays/settings.py
+++ krddevdays/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 
     'users',
     'events',
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -80,3 +82,5 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # QTickets
 QTICKETS_ENDPOINT = os.environ.get('QTICKETS_ENDPOINT', '')
 QTICKETS_TOKEN = os.environ.get('QTICKETS_TOKEN', '')
+
+CORS_ORIGIN_ALLOW_ALL=True
