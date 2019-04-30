d560dec7b58ce815c97b7902019f6fd6b5892fa7
TyVik <tyvik8@gmail.com>
2019-04-30 20:40:40 +0300

    Подключил чтение настроек из `.env`.

--- krddevdays/wsgi.py
+++ krddevdays/wsgi.py
@@ -9,7 +9,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
+import dotenv
 from django.core.wsgi import get_wsgi_application
+
+dotenv.load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'krddevdays.settings')
 
