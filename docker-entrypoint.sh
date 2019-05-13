040aef82ac7a75460d1febf9a0bec519b6525462
sergeibershadsky <sergei.ershadsky@3d4medical.com>
2019-05-13 17:08:15 +0300

    bind on everything

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ echo "Apply database migration"
 python3 manage.py migrate --noinput
 
 echo "Starting server"
-gunicorn krddevdays.wsgi:application --bind 127.0.0.1:8000 --timeout 60
+gunicorn krddevdays.wsgi:application --bind 0.0.0.0:8000 --timeout 60
