87c777471530e62233b6a181684aca3dcf7c18d3
sergeibersadsky <sergeibershadsky@gmail.com>
2019-08-11 21:26:34 +0300

    lower usernames

--- users/forms.py
+++ users/forms.py
@@ -8,6 +8,11 @@ class UserCreationForm(DjangoUserCreationForm):
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=150)
 
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if username:
+            return username.lower()
+
     class Meta(DjangoUserCreationForm.Meta):
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'last_name')
