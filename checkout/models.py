9f5a702c15f6253a4d0a7a9540a833423f5e3acc
TyVik <tyvik8@gmail.com>
2019-08-16 00:14:07 +0300

    Добавил поля из ответа QTickets.

--- checkout/models.py
+++ checkout/models.py
@@ -9,15 +9,25 @@ class ContactsMixin(models.Model):
     email = models.EmailField(db_index=True)
     first_name = models.CharField(max_length=30, null=True, blank=True)
     last_name = models.CharField(max_length=150, null=True, blank=True)
+    qticket_id = models.PositiveIntegerField(db_index=True)
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         abstract = True
+
+    def _unpack(self, response, fields):
+        for key, mapping in fields.items():
+            setattr(self, key, response[mapping])
 
 
 class Order(ContactsMixin, models.Model):
     event = models.ForeignKey(Event, on_delete=models.PROTECT)
     response = JSONField(default=dict)
-    qticket_id = models.PositiveIntegerField(db_index=True)
+    payed_at = models.DateTimeField(null=True, blank=True)
+
+    qticket_fields = {item: item for item in ('created_at', 'updated_at', 'deleted_at', 'payed_at')}
 
     def __str__(self):
         return f'{self.id} ({self.email})'
@@ -31,6 +41,7 @@ class Order(ContactsMixin, models.Model):
         except Order.DoesNotExist:
             order = Order(event=event, qticket_id=item['id'])
         order.response = item
+        order._unpack(item, Order.qticket_fields)
         order.email = client['email']
         if client['details']:
             order.first_name = client['details']['name']
@@ -47,8 +58,7 @@ class Order(ContactsMixin, models.Model):
             if original.email != ticket['client_email']:
                 original.email = ticket['client_email']
                 original.user = None
-            original.first_name = ticket['client_name']
-            original.last_name = ticket['client_surname']
+            original._unpack(ticket, Ticket.qticket_fields)
             original.save()
         Ticket.objects.filter(qticket_id__in=list(to_remove)).delete()
 
@@ -56,7 +66,19 @@ class Order(ContactsMixin, models.Model):
 class Ticket(ContactsMixin, models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='tickets')
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, related_name='tickets')
-    qticket_id = models.PositiveIntegerField(db_index=True)
+    pdf_url = models.URLField(null=True, blank=True)
+    passbook_url = models.URLField(null=True, blank=True)
+    type = models.CharField(max_length=40, null=True, blank=True)
+    price = models.PositiveIntegerField(null=True, blank=True)
+
+    qticket_fields = {
+        **{item: item for item in ('created_at', 'updated_at', 'deleted_at', 'pdf_url', 'passbook_url', 'price')},
+        **{
+            'first_name': 'client_name',
+            'last_name': 'client_surname',
+            'type': 'seat_id',
+        }
+    }
 
     def __str__(self):
         return f'Order {self.order_id}/{self.email}'
--- checkout/serializers.py
+++ checkout/serializers.py
@@ -5,8 +5,10 @@ from checkout.models import Ticket
 
 class UserTicketsSerializer(serializers.ModelSerializer):
     event_id = serializers.IntegerField(source='order.event_id')
+    payed_at = serializers.DateTimeField(source='order.payed_at')
+    number = serializers.IntegerField(source='qticket_id')
 
     class Meta:
         model = Ticket
-        fields = ('first_name', 'last_name', 'email', 'event_id')
+        fields = ('first_name', 'last_name', 'email', 'event_id', 'created_at', 'updated_at', 'deleted_at', 'payed_at', 'pdf_url', 'passbook_url', 'type', 'number', 'price')
         read_only_fields = ('first_name', 'last_name', 'email', 'event_id')
